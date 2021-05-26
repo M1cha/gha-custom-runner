93cb09c5ca93ee42ab92f0e4d755893b5337311d
Michael Zimmermann <sigmaepsilon92@gmail.com>
2021-05-26 22:30:40 +0200

    add support for workerBinary configuration
    With self-hosted runners all jobs usually have full access to the runner
    directory. You can put the runner in a VM or container and use `--once` to
    restart with a clean environment after each job but that doesn't prevent jobs
    from doing either of the following:
    - access runner secrets like `.credentials_rsaparams` to then take over the
      runner
    - modify the listener to nuke `--once` and manipulate future jobs.
    
    To fix that issue I'm running `Runner.Listener` in the host environment while
    running `Runner.Worker` in a VM or container.
    That is easily possible because the only way of communication between the two
    are unnamed pipes which can easily be forwarded into another environment.
    
    In my specific setup I set the config `workerBinary` to an absolute path outside
    of the runner installation. That binary then starts a qemu VM, forwards the
    pipes into it, places a copy of the runner installation into the VM and starts
    `Runner.Worker` with the forwarded pipes as the arguments.

--- src/Runner.Listener/CommandSettings.cs
+++ src/Runner.Listener/CommandSettings.cs
@@ -50,7 +50,8 @@ namespace GitHub.Runner.Listener
                     Constants.Runner.CommandLine.Args.UserName,
                     Constants.Runner.CommandLine.Args.WindowsLogonAccount,
                     Constants.Runner.CommandLine.Args.WindowsLogonPassword,
-                    Constants.Runner.CommandLine.Args.Work
+                    Constants.Runner.CommandLine.Args.Work,
+                    Constants.Runner.CommandLine.Args.WorkerBinary
                 },
             // Valid remove flags and args
             [Constants.Runner.CommandLine.Commands.Remove] =
@@ -333,6 +334,11 @@ namespace GitHub.Runner.Listener
         public string GetMonitorSocketAddress()
         {
             return GetArg(Constants.Runner.CommandLine.Args.MonitorSocketAddress);
+        }
+
+        public string GetWorkerBinary()
+        {
+            return GetArg(Constants.Runner.CommandLine.Args.WorkerBinary);
         }
 
         // This is used to find out the source from where the Runner.Listener.exe was launched at the time of run
