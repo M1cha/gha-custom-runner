actions/runner

add support for workerBinary configuration
With self-hosted runners all jobs usually have full access to the runner
directory. You can put the runner in a VM or container and use `--once` to
restart with a clean environment after each job but that doesn't prevent jobs
from doing either of the following:
- access runner secrets like `.credentials_rsaparams` to then take over the
  runner
- modify the listener to nuke `--once` and manipulate future jobs.

To fix that issue I'm running `Runner.Listener` in the host environment while
running `Runner.Worker` in a VM or container.
That is easily possible because the only way of communication between the two
are unnamed pipes which can easily be forwarded into another environment.

In my specific setup I set the config `workerBinary` to an absolute path outside
of the runner installation. That binary then starts a qemu VM, forwards the
pipes into it, places a copy of the runner installation into the VM and starts
`Runner.Worker` with the forwarded pipes as the arguments.

src/Runner.Listener/JobDispatcher.cs
@@ -36,6 +36,7 @@ namespace GitHub.Runner.Listener
     {
         private readonly Lazy<Dictionary<long, TaskResult>> _localRunJobResult = new Lazy<Dictionary<long, TaskResult>>();
         private int _poolId;
+        private string _workerBinary;
         RunnerSettings _runnerSetting;
         private static readonly string _workerProcessName = $"Runner.Worker{IOUtil.ExeExtension}";
 
@@ -57,6 +58,7 @@ namespace GitHub.Runner.Listener
             var configurationStore = hostContext.GetService<IConfigurationStore>();
             _runnerSetting = configurationStore.GetSettings();
             _poolId = _runnerSetting.PoolId;
+            _workerBinary = _runnerSetting.WorkerBinary;
 
             int channelTimeoutSeconds;
             if (!int.TryParse(Environment.GetEnvironmentVariable("GITHUB_ACTIONS_RUNNER_CHANNEL_TIMEOUT") ?? string.Empty, out channelTimeoutSeconds))
@@ -426,6 +428,9 @@ namespace GitHub.Runner.Listener
                                 HostContext.WritePerfCounter("StartingWorkerProcess");
                                 var assemblyDirectory = HostContext.GetDirectory(WellKnownDirectory.Bin);
                                 string workerFileName = Path.Combine(assemblyDirectory, _workerProcessName);
+                                if (!String.IsNullOrEmpty(_workerBinary)) {
+                                    workerFileName = _workerBinary;
+                                }
                                 workerProcessTask = processInvoker.ExecuteAsync(
                                     workingDirectory: assemblyDirectory,
                                     fileName: workerFileName,

M1cha/gha-custom-runner

src/Runner.Listener/JobDispatcher.cs
@@ -42,6 +42,7 @@ namespace GitHub.Runner.Listener
 
         IConfigurationStore _configurationStore;
 
+        private string _workerBinary;
         RunnerSettings _runnerSettings;
         private static readonly string _workerProcessName = $"Runner.Worker{IOUtil.ExeExtension}";
 
@@ -63,6 +64,7 @@ namespace GitHub.Runner.Listener
             _configurationStore = hostContext.GetService<IConfigurationStore>();
             _runnerSettings = _configurationStore.GetSettings();
             _poolId = _runnerSettings.PoolId;
+            _workerBinary = _runnerSettings.WorkerBinary;
 
             int channelTimeoutSeconds;
             if (!int.TryParse(Environment.GetEnvironmentVariable("GITHUB_ACTIONS_RUNNER_CHANNEL_TIMEOUT") ?? string.Empty, out channelTimeoutSeconds))
@@ -434,6 +436,9 @@ namespace GitHub.Runner.Listener
                                 HostContext.WritePerfCounter("StartingWorkerProcess");
                                 var assemblyDirectory = HostContext.GetDirectory(WellKnownDirectory.Bin);
                                 string workerFileName = Path.Combine(assemblyDirectory, _workerProcessName);
+                                if (!String.IsNullOrEmpty(_workerBinary)) {
+                                    workerFileName = _workerBinary;
+                                }
                                 workerProcessTask = processInvoker.ExecuteAsync(
                                     workingDirectory: assemblyDirectory,
                                     fileName: workerFileName,